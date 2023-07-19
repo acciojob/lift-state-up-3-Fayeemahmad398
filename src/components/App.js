dbc09644287144110f98a8371038b8e18874ae8b
Fayeemahmad398 <fayeemahmad0@gmail.com>
2023-07-19 13:47:16 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React from "react";
-import "./../styles/App.css";
-import Parent from "./Parent";
+import React, { useState } from "react";
+import './../styles/App.css';
 
 const App = () => {
-  return (
-    <>
-      <Parent />
-    </>
-  );
-};
+  const [selectedOption, setSelectedOption] = useState("")
 
-export default App;
+  return (
+    <div className="parent">
+      {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+
+      <div className="child1">
+        <h1>Child Component 1</h1>
+        <button onClick={() => setSelectedOption("Option 1")}>Option 1</button>
+      </div>
+
+      <div className="child2">
+        <h1>Child Component 2</h1>
+        <button onClick={() => setSelectedOption("Option 2")}>Option 2</button>
+      </div>
+      <p>Selected Option: {selectedOption}</p>
+    </div>
+  )
+}
+
+export default App
